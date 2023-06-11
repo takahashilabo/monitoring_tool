2d1524e38f987cd60cdb67cca1bd8c14511f7b24
Keiichi Takahashi <keiichi.takahashi@gmail.com>
2023-06-11 22:20:20 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 if [ -e ./config/initializers ]; then
   cd ./config/initializers; curl -O https://raw.githubusercontent.com/takahashilabo/monitoring_tool/main/monitoring.rb
 else
-  echo -e "\e[31mエラー：Railsディレクトリ内で実行してください\e[m"
+  echo "エラー：Railsディレクトリ内で実行してください"
 fi
