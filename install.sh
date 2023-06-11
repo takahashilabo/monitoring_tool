44bcc42fa35a923556c21d58f6be391f860a7479
Keiichi Takahashi <keiichi.takahashi@gmail.com>
2023-06-11 21:52:07 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [ -e ./config/initializers ]; then
-  cd ./config/initializers; curl -O https://github.com/takahashilabo/monitoring_tool/blob/main/monitoring.rb
+  cd ./config/initializers; curl -O https://raw.githubusercontent.com/takahashilabo/monitoring_tool/main/monitoring.rb
 fi
