d1c438e32b7ac533ab8b17d46176bbcc9040982a
Keiichi Takahashi <keiichi.takahashi@gmail.com>
2023-06-16 17:08:48 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 if [ -e ./config/initializers ]; then
   cd ./config/initializers; curl -O https://raw.githubusercontent.com/takahashilabo/monitoring_tool/main/monitoring.rb
-  echo "config/initializers/monitoring.rb に学籍番号を追加してください"
 else
   printf "\e[31mエラー：Railsディレクトリ内で実行してください\e[m\n"
 fi
